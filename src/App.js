49c0095f277cf1a2f4c166ab6cb594680f543ca9
programmermohaimin <you@example.com>
2022-09-06 20:09:09 +0600

    creating a quize app using react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Question from "./components/question";
+import Option from "./components/options";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Question />
+      <Option />
+      
     </div>
   );
 }
